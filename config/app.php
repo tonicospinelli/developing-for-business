d2bf3f76051ed2e1242ec8514e495a31a8ac2366
Antonio Spinelli <tonicospinelli85@gmail.com>
2016-06-14 23:19:36 -0300

    refactoring: isolating product model
    turn product functions reusable and decoupled from 'controller'

---
+++ config/app.php
@@ -0,0 +1,5 @@
+<?php
+
+define('DATABASE_DSN', 'sqlite:' . __DIR__ . '/../data/business.db');
+define('DATABASE_USERNAME', null);
+define('DATABASE_PASSWORD', null);
--- lib/functions.php
+++ lib/functions.php
@@ -1,37 +1,6 @@
 <?php
 
 /**
- * validate wish list data
- * @param array $data
- * @return bool
- */
-function isValidProduct(array $data)
-{
-    if (!isset($data['name']) || !assert(is_string($data['name']) !== false)) {
-        return false;
-    }
-    if (!isset($data['stock']) || !assert(is_numeric($data['stock']))) {
-        return false;
-    }
-    return true;
-}
-
-/**
- * Gets wish list data.
- * @param array $data
- * @return array
- */
-function getProduct(array $data)
-{
-    return array(
-        'id' => (isset($data['id']) ? $data['id'] : null),
-        'name' => $data['name'],
-        'price' => $data['price'],
-        'stock' => $data['stock']
-    );
-}
-
-/**
  * Creates a url to remove product from wish list
  * @param int $id
  * @return string

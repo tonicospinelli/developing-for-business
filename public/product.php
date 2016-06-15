15a6adee84774b9e0ed916e57c8dd8be251bc28b
Antonio Spinelli <tonicospinelli85@gmail.com>
2016-06-14 22:42:41 -0300

    refactoring: isolating view
    the HTML code is now moved to other path,
    which is primarily as HTML file that uses a template like PHP syntax

--- public/product.php
+++ public/product.php
@@ -22,37 +22,5 @@ if (isset($_POST['submit']) && isValidProduct($_POST['product'])) {
 $stm = $db->prepare('SELECT * FROM products');
 $stm->execute();
 $products = $stm->fetchAll(PDO::FETCH_ASSOC);
-?>
-<html>
-<head></head>
-<body>
-<?php if (null !== $errormsg): ?>
-    <div class="alert error"><?php echo $errormsg; ?> </div>
-<?php elseif (isset($product)): ?>
-    <div class="alert success"><?php echo $successmsg; ?></div>
-<?php endif; ?>
-<h3>Products</h3>
-<table>
-    <thead>
-    <tr>
-        <th>ID</th>
-        <th>PRODUCT</th>
-        <th>UNIT PRICE</th>
-        <th>STOCK</th>
-        <th>ACTIONS</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($products as $product): ?>
-        <tr>
-            <td><?php echo $product['id']; ?> </td>
-            <td><?php echo $product['name']; ?> </td>
-            <td><?php echo $product['unit_price']; ?> </td>
-            <td><?php echo $product['stock']; ?> </td>
-            <td><?php echo removeUrl('product.php', $product['id']); ?> </td>
-        </tr>
-    <?php endforeach; ?>
-    </tbody>
-</table>
-</body>
-</html>
+
+include __DIR__ . '/../templates/products/list.php';

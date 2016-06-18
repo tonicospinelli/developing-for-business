1b3087ebdc45a468d6ef2a9ac0e50df7fd23d469
Antonio Spinelli <tonicospinelli85@gmail.com>
2016-06-17 23:14:31 -0300

    refactoring: isolating application layout
    the main layout of application is extendable now

---
+++ templates/layout.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo $title ?></title>
+    </head>
+    <body>
+        <?php echo $content ?>
+    </body>
+</html>
--- templates/wishlists/list.php
+++ templates/wishlists/list.php
@@ -1,35 +1,36 @@
-<html>
-<head></head>
-<body>
-<?php if (null !== $errormsg): ?>
-    <div class="alert error"><?php echo $errormsg; ?> </div>
-<?php elseif (isset($wishItem)): ?>
-    <div class="alert success"><?php echo $successmsg; ?></div>
-<?php endif; ?>
-<h3>My Wish List</h3>
-<table>
-    <thead>
-    <tr>
-        <th>ID</th>
-        <th>PRODUCT</th>
-        <th>STATUS</th>
-        <th>ACTIONS</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($wishlist as $wish): ?>
+<?php $title = 'My Wish List' ?>
+
+<?php ob_start() ?>
+    <h3><?php echo $title;?></h3>
+    <?php if (null !== $errormsg): ?>
+        <div class="alert error"><?php echo $errormsg; ?> </div>
+    <?php elseif (isset($wishItem)): ?>
+        <div class="alert success"><?php echo $successmsg; ?></div>
+    <?php endif; ?>
+    <table>
+        <thead>
         <tr>
-            <td><?php echo $wish['id']; ?> </td>
-            <td><?php echo $wish['product_name']; ?> </td>
-            <?php if ($wish['product_stock'] == 0): ?>
-                <td>Not Available</td>
-            <?php else: ?>
-                <td>Available</td>
-            <?php endif; ?>
-            <td><?php echo removeUrl('wishlist.php', $wish['id'], ['email' => $_GET['email']]); ?> </td>
+            <th>ID</th>
+            <th>PRODUCT</th>
+            <th>STATUS</th>
+            <th>ACTIONS</th>
         </tr>
-    <?php endforeach; ?>
-    </tbody>
-</table>
-</body>
-</html>
+        </thead>
+        <tbody>
+        <?php foreach ($wishlist as $wish): ?>
+            <tr>
+                <td><?php echo $wish['id']; ?> </td>
+                <td><?php echo $wish['product_name']; ?> </td>
+                <?php if ($wish['product_stock'] == 0): ?>
+                    <td>Not Available</td>
+                <?php else: ?>
+                    <td>Available</td>
+                <?php endif; ?>
+                <td><?php echo removeUrl('wishlist.php', $wish['id'], ['email' => $_GET['email']]); ?> </td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+<?php $content = ob_get_clean() ?>
+
+<?php include __DIR__.'/../layout.php' ?>
